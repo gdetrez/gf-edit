37b382301d4d68dbe95b368fd48e2c53419b39f8
Grégoire Détrez <gregoire.detrez@gu.se>
2013-08-20 08:51:52 +0200

    Add a simple controller with containing the 2 files

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,14 +3,21 @@
 /* jasmine specs for controllers go here */
 
 describe('controllers', function(){
-  beforeEach(module('myApp.controllers'));
-
-
-  it('should ....', inject(function() {
-    //spec body
-  }));
+  beforeEach(module('controllers'));
 
   it('should ....', inject(function() {
     //spec body
   }));
+
+  describe('Editor controllers', function() {
+    describe('EditorCtrl', function(){
+      it('hould create "files" model with 2 files', inject(function($rootScope, $controller) {
+        //spec body
+        var scope = $rootScope.$new(),
+            ctrl = $controller("EditorCtrl", {$scope: scope });
+        expect(scope.data['MyAbstract.gf']).not.toBe(null);
+        expect(scope.data['MyConcrete.gf']).not.toBe(null);
+      }));
+    });
+  });
 });
