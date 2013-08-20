d0e880dd35ab34552066d3e46152d5b21cb9fc54
Grégoire Détrez <gregoire.detrez@gu.se>
2013-08-20 08:51:52 +0200

    Add a simple controller with containing the 2 files

--- js/controllers.js
+++ js/controllers.js
@@ -2,9 +2,14 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
+angular.module('controllers', []).
+  controller('EditorCtrl', ['$scope', function($scope) {
+    $scope.data = {
+      'MyAbstract.gf':
+        'abstract MyAbstract = {\n  cat Greeting ;\n}',
+      'MyConcrete.gf':
+        'concrete MyContrete of MyAbstract = {\n  lincat Greeting = str ;\n  lin hi = \"Hello world!\" ;\n}'
+      };
   }])
   .controller('MyCtrl2', [function() {
 
