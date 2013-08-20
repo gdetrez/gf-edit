b25b770b2f1e1bdc614890c2042c1bcde60580f9
Grégoire Détrez <gregoire.detrez@gu.se>
2013-08-20 11:10:31 +0200

    Add login view

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('GFAce', ['myApp.filters', 'GFAce.services', 'myApp.directives', 'GFAce.controllers', 'ui.ace']).
   config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginCtrl'});
     $routeProvider.when('/editor/:file', {templateUrl: 'partials/editor.html', controller: 'EditorCtrl'});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.otherwise({redirectTo: '/login'});
   }]);
